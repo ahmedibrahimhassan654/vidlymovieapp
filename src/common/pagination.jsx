a1fda0b6a2202536f3ae0b7c0db54bb767bff2b1
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-07-21 23:30:03 +0200

    4-pagination displaying pages

--- src/common/pagination.jsx
+++ src/common/pagination.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
 
 
-const Pagination = () => {
-    return (<nav aria-label="Page navigation example">
-        <ul className="pagination">
-            <li className="page-item"><a className="page-link" href="/#">Previous</a></li>
-            <li className="page-item"><a className="page-link" href="/#">1</a></li>
-            <li className="page-item"><a className="page-link" href="/#">2</a></li>
-            <li className="page-item"><a className="page-link" href="/#">3</a></li>
-            <li className="page-item"><a className="page-link" href="/#">Next</a></li>
-        </ul>
-    </nav>);
+
+const Pagination = (props) => {
+    const { itemCount, pageSize } = props;
+
+    const pagesCount =Math.ceil( itemCount / pageSize);
+    console.log(pagesCount);
+    if (pagesCount === 1) return null;
+    const pages = _.range(1, pagesCount + 1);
+
+
+    return (
+        <nav aria-label="Page navigation example">
+            <ul className="pagination">
+                {pages.map(page => (
+                    <li key={page} className="page-item">
+                        <a className="page-link" href="/#">{page}</a>
+
+                    </li>
+                ))}
+
+            </ul>
+        </nav>
+    );
 
 };
 
--- src/movies/Movies.jsx
+++ src/movies/Movies.jsx
@@ -5,7 +5,8 @@ import Pagination from '../common/pagination';
 
 class Movies extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4
     };
     deleteMovie = (movie) => {
         const newmovies = this.state.movies.filter(m => m._id !== movie._id);
@@ -18,14 +19,17 @@ class Movies extends Component {
         movies[index].liked = !movies[index].liked;
         this.setState({ movies });
     };
+    handlePageChange = (page) => {
+        console.log(page);
+    };
     render() {
-
-        if (this.state.movies.length === 0)
+        const {length:count}=this.state.movies
+        if (count === 0)
             return <p>there is no movie yet on database </p>;
 
         return (
             <React.Fragment>
-                <p>the number of movies equal {this.state.movies.length}</p>
+                <p>the number of movies equal {count}</p>
                 <table className="table table-hover " >
                     <thead>
                         <tr>
@@ -68,7 +72,11 @@ class Movies extends Component {
 
                     </tbody>
                 </table >
-                <Pagination />
+                <Pagination
+                    itemCount={count}
+                    pageSize={10}
+                    onPageChange={this.handlePageChange}
+                />
             </React.Fragment>
 
         );
